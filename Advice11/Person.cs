e781bab983f587b9b78f96123208adb892162b76
bomir.wang <jinchunw385@gmail.com>
2021-11-08 12:06:51 +0800

    enhance code

--- Advice11/Person.cs
+++ Advice11/Person.cs
@@ -1,6 +1,8 @@
-﻿namespace Advice11
+﻿using System;
+
+namespace Advice11
 {
-    class Person
+    class Person : IEquatable<Person>
     {
         public string ID { get; private set; }
         public Person(string id)
@@ -11,6 +13,16 @@
         public override bool Equals(object obj)
         {
             return ID == (obj as Person).ID;
+        }
+
+        public override int GetHashCode()
+        {
+            return (System.Reflection.MethodBase.GetCurrentMethod().DeclaringType.FullName + "#" + this.ID).GetHashCode();
+        }
+
+        public bool Equals(Person other)
+        {
+            return ID == other.ID;
         }
     }
 }
--- Advice11/Program.cs
+++ Advice11/Program.cs
@@ -1,16 +1,35 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace Advice11
 {
     class Program
     {
+        static Dictionary<Person, PersonMoreInfo> PersonValues = new Dictionary<Person, PersonMoreInfo>();
         static void Main(string[] args)
         {
-            object a = new Person("NB123");
-            object b = new Person("NB123");
-
-            Console.WriteLine(a == b);
-            Console.WriteLine(a.Equals(b));
+            //AddPerson();
+            //Person bomir = new Person("morn423");
+            //// Console.WriteLine(bomir.GetHashCode());
+            //Console.WriteLine(PersonValues.ContainsKey(bomir));
+            string str1 = "NB0903100006";
+            string str2 = "NB0904140001";
+            Console.WriteLine(str1.GetHashCode());
+            Console.WriteLine(str2.GetHashCode());
         }
+
+        static void AddPerson()
+        {
+            Person bomir = new Person("morn423");
+            PersonMoreInfo bomirValue = new PersonMoreInfo() { SomeInfo = "Bomir's Info" };
+            PersonValues.Add(bomir, bomirValue);
+            // Console.WriteLine(bomir.GetHashCode());
+            Console.WriteLine(PersonValues.ContainsKey(bomir));
+        }
+    }
+
+    public class PersonMoreInfo
+    {
+        public string SomeInfo { get; set; }
     }
 }
