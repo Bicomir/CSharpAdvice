5f15d00f8a8a06bb1101dffb51a3558e59436fec
bomir.wang <jinchunw385@gmail.com>
2021-11-08 12:30:04 +0800

    add Advice12 first method

---
+++ Advice13/Person.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Advice13
+{
+    class Person : IFormattable
+    {
+        public string IDCode { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        // 实现接口IFormattable的方法ToString()
+        public string ToString(string format, IFormatProvider formatProvider)
+        {
+            switch (format)
+            {
+                case "ch":
+                    return this.ToString();
+                case "eg":
+                    return string.Format("{0} {1}", FirstName, LastName);
+                default:
+                    return this.ToString();
+            }
+        }
+
+        // 重写Object.ToString()
+        public override string ToString()
+        {
+            return string.Format("{0} {1}", LastName, FirstName);
+        }
+    }
+}
---
+++ Advice13/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Advice13
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person person = new Person() { FirstName="Bomir", LastName="Wang", IDCode="Morn423" };
+            Console.WriteLine(person);
+            Console.WriteLine(person.ToString("ch", null));
+            Console.WriteLine(person.ToString("eg", null));
+        }
+    }
+}
