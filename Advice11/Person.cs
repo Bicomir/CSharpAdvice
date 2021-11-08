20d9855598ed536a21aa0768bf9a3f39bf1c2065
bomir.wang <jinchunw385@gmail.com>
2021-11-08 10:44:08 +0800

    add Advice11 code

---
+++ Advice11/Person.cs
@@ -0,0 +1,16 @@
+﻿namespace Advice11
+{
+    class Person
+    {
+        public string ID { get; private set; }
+        public Person(string id)
+        {
+            this.ID = id;
+        }
+
+        public override bool Equals(object obj)
+        {
+            return ID == (obj as Person).ID;
+        }
+    }
+}
---
+++ Advice11/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Advice11
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            object a = new Person("NB123");
+            object b = new Person("NB123");
+
+            Console.WriteLine(a == b);
+            Console.WriteLine(a.Equals(b));
+        }
+    }
+}
