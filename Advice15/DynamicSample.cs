2778e815e28ea547fea903a48481361cf4781297
bomir.wang <jinchunw385@gmail.com>
2021-11-09 13:09:23 +0800

    add Advice15 1st case

---
+++ Advice15/DynamicSample.cs
@@ -0,0 +1,11 @@
+﻿namespace Advice15
+{
+    class DynamicSample
+    {
+        public string Name { get; set; }
+        public int Add(int a, int b)
+        {
+            return a + b;
+        }
+    }
+}
---
+++ Advice15/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Advice15
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DynamicSample dynamicSample = new DynamicSample();
+            var addMethod = typeof(DynamicSample).GetMethod("Add");
+            int re = (int)addMethod.Invoke(dynamicSample, new object[] { 1, 2 });
+            Console.WriteLine(re);
+        }
+    }
+}
