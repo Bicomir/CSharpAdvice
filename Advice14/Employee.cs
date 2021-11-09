c9263123d425dc5e6dfb2bf78d4198b87452a21e
bomir.wang <jinchunw385@gmail.com>
2021-11-09 10:52:42 +0800

    add Advice14 deepClone

--- Advice14/Employee.cs
+++ Advice14/Employee.cs
@@ -1,7 +1,11 @@
 ﻿using System;
+using System.IO;
+using System.Runtime.Serialization;
+using System.Runtime.Serialization.Formatters.Binary;
 
 namespace Advice14
 {
+    [Serializable]
     class Employee : ICloneable
     {
         public string IDCode { get; set; }
@@ -9,10 +13,17 @@ namespace Advice14
         public Department Department { get; set; }
         public object Clone()
         {
-            return MemberwiseClone();
+            using (Stream objectstram = new MemoryStream())
+            {
+                IFormatter formatter = new BinaryFormatter();
+                formatter.Serialize(objectstram, this);
+                objectstram.Seek(0, SeekOrigin.Begin);
+                return formatter.Deserialize(objectstram) as Employee;
+            }
         }
     }
 
+    [Serializable]
     public class Department
     {
         public string Name { get; set; }
