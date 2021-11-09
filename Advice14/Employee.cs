cb04a973fa04c6a1c490463c562df1cc965c10c2
bomir.wang <wangjinqun@skieer.com>
2021-11-09 10:19:46 +0800

    Add Advice14 light copy
    Advice14

---
+++ Advice14/Employee.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Advice14
+{
+    class Employee : ICloneable
+    {
+        public string IDCode { get; set; }
+        public int Age { get; set; }
+        public Department Department { get; set; }
+        public object Clone()
+        {
+            return MemberwiseClone();
+        }
+    }
+
+    public class Department
+    {
+        public string Name { get; set; }
+        public override string ToString()
+        {
+            return Name;
+        }
+    }
+}
---
+++ Advice14/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Advice14
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee bomir = new Employee()
+            {
+                IDCode = "morn423",
+                Age = 30,
+                Department = new Department() { Name = "Dep1" }
+            };
+
+            Employee kiki = bomir.Clone() as Employee;
+            Console.WriteLine(kiki.IDCode);
+            Console.WriteLine(kiki.Age);
+            Console.WriteLine(kiki.Department);
+
+            Console.WriteLine("开始改变bomir的值: ");
+            bomir.IDCode = "morn456";
+            bomir.Age = 22;
+            bomir.Department.Name = "Dep2";
+
+            Console.WriteLine(kiki.IDCode);
+            Console.WriteLine(kiki.Age);
+            Console.WriteLine(kiki.Department);
+        }
+    }
+}
