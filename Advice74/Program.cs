3515c3ddcd5acd18f8c14f2a0b5be45bf6c342f4
bomir.wang <jinchunw385@gmail.com>
2021-11-10 10:01:19 +0800

    Add Advice74 code

---
+++ Advice74/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Threading;
+
+namespace Advice74
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Thread t = new Thread(() =>
+            {
+                Console.WriteLine("线程开始工作...");
+                // 省略工作代码
+              //  Console.ReadKey();
+                Console.WriteLine("线程结束");
+            });
+
+            // 注意， 默认为false
+            t.IsBackground = true;
+            t.Start();
+            Console.WriteLine("主线程完毕!");
+        }
+    }
+}
