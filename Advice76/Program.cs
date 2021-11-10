ea4532ae13dbfe90e2a1dadce69451a792bd3e0b
bomir.wang <jinchunw385@gmail.com>
2021-11-10 18:37:08 +0800

    Add Advice76 code

---
+++ Advice76/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Threading;
+
+namespace Advice76
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            long t1Num = 0;
+            long t2Num = 0;
+            CancellationTokenSource cts = new CancellationTokenSource();
+
+            Thread t1 = new Thread(() => 
+            {
+                while (true && !cts.Token.IsCancellationRequested)
+                {
+                    t1Num++;
+                }
+            });
+            t1.IsBackground = true;
+            t1.Priority = ThreadPriority.Highest;
+            t1.Start();
+
+            Thread t2 = new Thread(() =>
+            {
+                while (true && !cts.Token.IsCancellationRequested)
+                {
+                    t2Num++;
+                }
+            });
+
+            t2.IsBackground = true;
+            t2.Start();
+
+            Console.ReadLine();
+
+            // 停止线程
+            cts.Cancel();
+            Console.WriteLine("t1Num:" + t1Num.ToString());
+            Console.WriteLine("t2Num:" + t2Num.ToString());
+
+        }
+    }
+}
