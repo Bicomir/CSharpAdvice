f174274e3a20ce8fea631d92957e593245ff2897
bomir.wang <jinchunw385@gmail.com>
2021-11-10 17:44:40 +0800

    Add Advice75 code enhancement

--- Advice75/Program.cs
+++ Advice75/Program.cs
@@ -10,15 +10,20 @@ namespace Advice75
         {
             for (int i = 0; i < 10; i++, _id++)
             {
-                Thread t = new Thread(() =>
-                {
-                    Console.WriteLine(string.Format("{0}:{1}", Thread.CurrentThread.Name, _id));
-                });
-                t.Name = string.Format("Thread{0}", i);
-                t.IsBackground = true;
-                t.Start();
+                NewMethod(i, _id);
             }
             Console.ReadLine();
+        }
+
+        private static void NewMethod(int i, int realTimeID)
+        {
+            Thread t = new Thread(() =>
+            {
+                Console.WriteLine(string.Format("{0}:{1}", Thread.CurrentThread.Name, realTimeID));
+            });
+            t.Name = string.Format("Thread{0}", i);
+            t.IsBackground = true;
+            t.Start();
         }
     }
 }
