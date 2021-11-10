3e2fa0c967a30a96b4b5ffdbea915925d05e7298
bomir.wang <jinchunw385@gmail.com>
2021-11-10 10:15:43 +0800

    Add Advice75 1st case code

---
+++ Advice75/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Threading;
+
+namespace Advice75
+{
+    internal class Program
+    {
+        static int _id = 0;
+        static void Main(string[] args)
+        {
+            for (int i = 0; i < 10; i++, _id++)
+            {
+                Thread t = new Thread(() =>
+                {
+                    Console.WriteLine(string.Format("{0}:{1}", Thread.CurrentThread.Name, _id));
+                });
+                t.Name = string.Format("Thread{0}", i);
+                t.IsBackground = true;
+                t.Start();
+            }
+            Console.ReadLine();
+        }
+    }
+}
