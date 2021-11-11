a3176325933fcd9270a7413a8cc99e7eac1f1bd2
bomir.wang <jinchunw385@gmail.com>
2021-11-11 10:49:45 +0800

    add Advice77 code

---
+++ Advice77/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Threading;
+
+namespace Advice77
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            CancellationTokenSource cts = new CancellationTokenSource();
+            Thread t = new Thread(() =>
+            {
+                while (true)
+                {
+                    if (cts.Token.IsCancellationRequested)
+                    {
+                        Console.WriteLine("线程被终止! ");
+                        break;
+                    }
+                    Console.WriteLine(DateTime.Now.ToString());
+                    Thread.Sleep(1000);
+
+                    //cts.Token.Register(() =>
+                    //{
+                    //    Console.WriteLine("工作线程被终止了.");
+                    //});
+                }
+            });
+            t.Start();
+            Console.ReadLine();
+            cts.Cancel();
+        }
+    }
+}
