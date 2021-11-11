b5996227fd3157ee6c6043368a8c98624f1ba625
bomir.wang <jinchunw385@gmail.com>
2021-11-11 12:18:36 +0800

    Add Advice78 code

---
+++ Advice78/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Threading;
+
+namespace Advice78
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 0; i < 200; i++)
+            {
+                Thread t = new Thread(() => 
+                {
+                    int j = i;
+                    while (true)
+                    {
+                        j++;
+                    }
+                    t.IsBackground = true;
+                    t.Start();
+                });
+            }
+
+            Thread.Sleep(5000);
+            Thread T201 = new Thread(() => 
+            {
+                while (true)
+                {
+                    Console.WriteLine("T201正在执行！");
+                }
+            });
+            T201.Start();
+            Console.ReadKey();
+        }
+    }
+}
