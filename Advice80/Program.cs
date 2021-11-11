f1932f75ee65f8302a5775ed5e67583bdebfa871
bomir.wang <jinchunw385@gmail.com>
2021-11-11 18:54:03 +0800

    Add Advice80 1st case code

---
+++ Advice80/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Advice80
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Task t = new Task(() => 
+            {
+                Console.WriteLine("任务开始工作...");
+
+                // 模拟工作过程
+                Thread.Sleep(5000);
+            });
+            t.Start();
+            t.ContinueWith((task) =>
+            {
+                Console.WriteLine("任务完成， 完成时候的状态为: ");
+                Console.WriteLine("IsCanceled={0}\tIsCompleted={1}\tIsFaulted={2}", task.IsCanceled, task.IsCompleted, task.IsFaulted);
+            });
+            Console.ReadLine();
+        }
+    }
+}
