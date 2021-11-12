22074ea8ccc2f8e89c38f4f7b6f73d16d63fae86
bomir.wang <jinchunw385@gmail.com>
2021-11-12 18:35:22 +0800

    Advice85 1st case code.

---
+++ Advice85/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace Advice85
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Task t = new Task(() => 
+            {
+                throw new Exception("任务并行时候产生的异常");
+            });
+            t.Start();
+
+            try
+            {
+                // 若有Result， 可求Result
+                t.Wait();
+            }
+            catch (AggregateException e)
+            {
+                foreach (var item in e.InnerExceptions)
+                {
+                    Console.WriteLine("ExceptionType: {0}{1} from {2}{3} Exception Content:{4}", item.GetType(), Environment.NewLine, item.Source, Environment.NewLine, item.Message);
+                }
+            }
+            Console.WriteLine("主线程马上结束!");
+            Console.ReadKey();
+        }
+    }
+}
