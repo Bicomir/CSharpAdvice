80b73fb1aa825fad67280c27007d7b176fe4fb83
bomir.wang <jinchunw385@gmail.com>
2021-11-12 09:42:56 +0800

    Advice80 2nd case code

--- Advice80/Program.cs
+++ Advice80/Program.cs
@@ -8,20 +8,34 @@ namespace Advice80
     {
         static void Main(string[] args)
         {
-            Task t = new Task(() => 
-            {
-                Console.WriteLine("任务开始工作...");
-
-                // 模拟工作过程
-                Thread.Sleep(5000);
-            });
+            CancellationTokenSource cts = new CancellationTokenSource();
+            Task<int> t = new Task<int>(() => Add(cts.Token), cts.Token);
             t.Start();
-            t.ContinueWith((task) =>
+            t.ContinueWith(TaskEnd);
+
+            // 等待按任意键取消任务
+            Console.ReadKey();
+            cts.Cancel();
+            Console.ReadKey();
+        }
+
+        private static void TaskEnd(Task<int> task)
+        {
+            Console.WriteLine("任务完成，完成时候的状态为: ");
+            Console.WriteLine("IsCanceled={0}\tIsCompleted={1}\tIsFaulted={2}", task.IsCanceled, task.IsCompleted, task.IsFaulted);
+            Console.WriteLine("任务的返回值为: {0}", task.Result);
+        }
+
+        private static int Add(CancellationToken ct)
+        {
+            Console.WriteLine("任务开始...");
+            int result = 0;
+            while (!ct.IsCancellationRequested)
             {
-                Console.WriteLine("任务完成， 完成时候的状态为: ");
-                Console.WriteLine("IsCanceled={0}\tIsCompleted={1}\tIsFaulted={2}", task.IsCanceled, task.IsCompleted, task.IsFaulted);
-            });
-            Console.ReadLine();
+                result++;
+                Thread.Sleep(1000);
+            }
+            return result;
         }
     }
 }
