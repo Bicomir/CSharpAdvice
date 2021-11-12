ed19de7595a3ccb88b0f2f491924b20606b387bd
bomir.wang <jinchunw385@gmail.com>
2021-11-12 11:06:37 +0800

    Advice81 1st case code

---
+++ Advice81/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace Advice81
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] nums = new int[] { 1, 2, 3, 4, 5};
+            Parallel.For(0, nums.Length, i => 
+            { 
+                Console.WriteLine("ArrayIndex{0} -> Element{1}", i, nums[i]);
+            });
+            Console.ReadKey();
+        }
+    }
+}
