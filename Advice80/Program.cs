d43777021b72f825d81f6d9f383daeb01bca2b83
bomir.wang <jinchunw385@gmail.com>
2021-11-12 10:47:52 +0800

    Advice80 4th case code

--- Advice80/Program.cs
+++ Advice80/Program.cs
@@ -9,46 +9,37 @@ namespace Advice80
         static void Main(string[] args)
         {
             CancellationTokenSource cts = new CancellationTokenSource();
-            Task<int> t = new Task<int>(() => AddCancelByThrow(cts.Token), cts.Token);
-            t.Start();
-            t.ContinueWith(TaskEndByCatch);
+            TaskFactory taskFactory = new TaskFactory();
+            Task[] tasks = new Task[]
+            {
+                taskFactory.StartNew(() => Add(cts.Token)),
+                taskFactory.StartNew(() => Add(cts.Token)),
+                taskFactory.StartNew(() => Add(cts.Token)),
+            };
 
-            // 等待按任意键取消任务
+            // CancellationToken.None, 指示TasksEnded不能被取消
+            taskFactory.ContinueWhenAll(tasks, TaskEnded, CancellationToken.None);
             Console.ReadKey();
             cts.Cancel();
             Console.ReadKey();
         }
 
-        private static void TaskEndByCatch(Task<int> task)
-        {
-            Console.WriteLine("任务完成，完成时候的状态为: ");
-            Console.WriteLine("IsCanceled={0}\tIsCompleted={1}\tIsFaulted={2}", task.IsCanceled, task.IsCompleted, task.IsFaulted);
-
-            try
-            {
-                Console.WriteLine("任务的返回值为: {0}", task.Result);
-            }
-            catch (AggregateException e)
-            {
-                e.Handle((err) => err is OperationCanceledException);
-            }
-        }
-
-        private static int AddCancelByThrow(CancellationToken ct)
+        private static int Add(CancellationToken ct)
         {
             Console.WriteLine("任务开始...");
             int result = 0;
-            while (true)
+            while (!ct.IsCancellationRequested)
             {
-                // ct.ThrowIfCancellationRequested();
-                if (result == 5)
-                {
-                    throw new Exception("error");
-                }
                 result++;
                 Thread.Sleep(1000);
             }
             return result;
         }
+
+        private static void TaskEnded(Task[] task)
+        {
+            Console.WriteLine("所有任务已完成！");
+        }
+
     }
 }
