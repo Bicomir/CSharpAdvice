0b9d038f6fa98c7a36b4150ca2522e43f96baf0d
bomir.wang <jinchunw385@gmail.com>
2021-11-12 10:13:02 +0800

    Advice80 3rd case code

--- Advice80/Program.cs
+++ Advice80/Program.cs
@@ -9,9 +9,9 @@ namespace Advice80
         static void Main(string[] args)
         {
             CancellationTokenSource cts = new CancellationTokenSource();
-            Task<int> t = new Task<int>(() => Add(cts.Token), cts.Token);
+            Task<int> t = new Task<int>(() => AddCancelByThrow(cts.Token), cts.Token);
             t.Start();
-            t.ContinueWith(TaskEnd);
+            t.ContinueWith(TaskEndByCatch);
 
             // 等待按任意键取消任务
             Console.ReadKey();
@@ -19,19 +19,28 @@ namespace Advice80
             Console.ReadKey();
         }
 
-        private static void TaskEnd(Task<int> task)
+        private static void TaskEndByCatch(Task<int> task)
         {
             Console.WriteLine("任务完成，完成时候的状态为: ");
             Console.WriteLine("IsCanceled={0}\tIsCompleted={1}\tIsFaulted={2}", task.IsCanceled, task.IsCompleted, task.IsFaulted);
-            Console.WriteLine("任务的返回值为: {0}", task.Result);
+
+            try
+            {
+                Console.WriteLine("任务的返回值为: {0}", task.Result);
+            }
+            catch (AggregateException e)
+            {
+                e.Handle((err) => err is OperationCanceledException);
+            }
         }
 
-        private static int Add(CancellationToken ct)
+        private static int AddCancelByThrow(CancellationToken ct)
         {
             Console.WriteLine("任务开始...");
             int result = 0;
-            while (!ct.IsCancellationRequested)
+            while (true)
             {
+                ct.ThrowIfCancellationRequested();
                 result++;
                 Thread.Sleep(1000);
             }
