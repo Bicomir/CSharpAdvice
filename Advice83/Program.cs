1cd7c4b301bd11cde9890f0b28823adc8455f436
bomir.wang <jinchunw385@gmail.com>
2021-11-12 17:27:33 +0800

    Advice83 1st case code

---
+++ Advice83/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Advice83
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] nums = { 1, 2, 3, 4 };
+            int total = 0;
+            Parallel.For<int>(0, nums.Length, () => 
+            {
+                return 1;    
+            }, (i, loopState, subtotal) =>
+            {
+                subtotal += nums[i];
+                return subtotal;
+            },
+            (x) => Interlocked.Add(ref total, x));
+
+            Console.WriteLine("total={0}", total);
+            Console.ReadKey();
+        }
+    }
+}
