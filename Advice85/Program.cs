0c18c209580eb15b8c27a5df9bb2b76b86b05ebb
bomir.wang <jinchunw385@gmail.com>
2021-11-12 18:55:15 +0800

    Advice85 2nd case code

--- Advice85/Program.cs
+++ Advice85/Program.cs
@@ -9,22 +9,27 @@ namespace Advice85
         {
             Task t = new Task(() => 
             {
-                throw new Exception("任务并行时候产生的异常");
+                throw new Exception("任务并行编码时候产生的未知异常");
             });
             t.Start();
 
+            Task ttEnd = t.ContinueWith((task) =>
+            {
+                throw task.Exception; 
+            }, TaskContinuationOptions.OnlyOnFaulted);
+
             try
             {
-                // 若有Result， 可求Result
-                t.Wait();
+                ttEnd.Wait();
             }
-            catch (AggregateException e)
+            catch (AggregateException err)
             {
-                foreach (var item in e.InnerExceptions)
+                foreach (var item in err.InnerExceptions)
                 {
-                    Console.WriteLine("ExceptionType: {0}{1} from {2}{3} Exception Content:{4}", item.GetType(), Environment.NewLine, item.Source, Environment.NewLine, item.Message);
+                    Console.WriteLine("ExceptionType: {0} {1} from {2} {3} Exception Content:{4}", item.InnerException.GetType(), Environment.NewLine, item.InnerException.Source, Environment.NewLine, item.InnerException.Message);
                 }
             }
+
             Console.WriteLine("主线程马上结束!");
             Console.ReadKey();
         }
