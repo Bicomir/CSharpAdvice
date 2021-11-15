e066e3b75f3948b96b25eec1d4dcee48c34d1b06
bomir.wang <jinchunw385@gmail.com>
2021-11-15 13:23:29 +0800

    Advice85 new case code

--- Advice85/Program.cs
+++ Advice85/Program.cs
@@ -3,35 +3,54 @@ using System.Threading.Tasks;
 
 namespace Advice85
 {
-    internal class Program
+    
+    
+
+    public class Program
     {
+        public class AggregateExceptionArgs : EventArgs
+        {
+            public AggregateException AggregateException { get; set; }
+        }
+
+        static event EventHandler<AggregateExceptionArgs> AggregateExceptionCatched;
         static void Main(string[] args)
         {
+            AggregateExceptionCatched += EventHandler<AggregateExceptionArgs>(Program_AggregateExceptionCatched);
             Task t = new Task(() => 
             {
-                throw new Exception("任务并行编码时候产生的未知异常");
+                try
+                {
+                    throw new Exception("任务并行编码时候产生的未知异常");
+                }
+                catch (Exception err)
+                {
+                    AggregateExceptionArgs errArgs = new AggregateExceptionArgs { AggregateException = new AggregateException(err) };
+                    AggregateExceptionCatched(null, errArgs);
+                }
+                
             });
             t.Start();
 
-            Task ttEnd = t.ContinueWith((task) =>
-            {
-                throw task.Exception; 
-            }, TaskContinuationOptions.OnlyOnFaulted);
-
-            try
-            {
-                ttEnd.Wait();
-            }
-            catch (AggregateException err)
-            {
-                foreach (var item in err.InnerExceptions)
-                {
-                    Console.WriteLine("ExceptionType: {0} {1} from {2} {3} Exception Content:{4}", item.InnerException.GetType(), Environment.NewLine, item.InnerException.Source, Environment.NewLine, item.InnerException.Message);
-                }
-            }
+          
 
             Console.WriteLine("主线程马上结束!");
             Console.ReadKey();
         }
+
+        private static EventHandler<T> EventHandler<T>(Action<object, T> program_AggregateExceptionCatched)
+        {
+            throw new NotImplementedException();
+        }
+
+        static void Program_AggregateExceptionCatched(object sender, AggregateExceptionArgs e)
+        {
+            foreach (var item in e.AggregateException.InnerExceptions)
+            {
+                Console.WriteLine("ExceptionType: {0} {1} from {2} {3} Exception Content:{4}", item.GetType(), Environment.NewLine, item.Source, Environment.NewLine, item.Message);
+            }
+        }
     }
+
+    
 }
