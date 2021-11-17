0e139b25db1c9a5952db715f2238c1ce5000cdb2
bomir.wang <jinchunw385@gmail.com>
2021-11-17 15:44:11 +0800

    Advice89 1st case code

---
+++ Advice88/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Diagnostics;
+using System.Threading.Tasks;
+
+namespace Advice88
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Stopwatch watch = new Stopwatch();
+            watch.Start();
+            DoInFor();
+            watch.Stop();
+
+            Console.WriteLine("Sync Cost time:{0}", watch.Elapsed);
+
+            watch.Restart();
+            DoInParalleFor();
+            watch.Stop();
+            Console.WriteLine("Parallel Cost time:{0}", watch.Elapsed);
+
+        }
+
+        static void DoSometing()
+        {
+            for (int i = 0; i < 10000000; i++)
+            {
+                i++;
+            }
+        }
+
+        static void DoInFor()
+        {
+            for (int i = 0; i < 200; i++)
+            {
+                DoSometing();
+            }
+        }
+
+        static void DoInParalleFor()
+        {
+            Parallel.For(0, 200, (i) => 
+            {
+                DoSometing();
+            });
+        }
+    }
+}
