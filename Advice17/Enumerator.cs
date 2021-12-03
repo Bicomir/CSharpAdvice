21c8c17718bee8160b0f9273598637fd96f8a0c2
bomir.wang <jinchunw385@gmail.com>
2021-12-03 11:05:14 +0800

    Advice17 1st case

---
+++ Advice17/Enumerator.cs
@@ -0,0 +1,69 @@
+﻿namespace Advice17
+{
+    // 要求所有迭代器全部实现该接口
+    interface IMyEnumerator
+    {
+        bool MoveNext();
+
+        object Current { get; }
+    }
+
+    // 要求所有的集合实现该接口
+    // 这样一来， 客户端就可以针对该接口编码， 而无需关注具体的实现
+    interface IMyEnumerable
+    {
+        IMyEnumerator GetEnumerator();
+
+        int Count { get; }
+    }
+
+    class MyList : IMyEnumerable
+    {
+        object[] items = new object[10];
+        IMyEnumerator myEnumerator;
+
+        public object this[int i]
+        {
+            get { return items[i]; }
+            set { this.items[i] = value; }
+        }
+
+        public int Count => items.Length;
+
+        public IMyEnumerator GetEnumerator()
+        {
+            if (myEnumerator == null)
+            {
+                myEnumerator = new MyEnumerator(this);
+            }
+            return myEnumerator;
+        }
+    }
+
+    class MyEnumerator : IMyEnumerator
+    {
+        int index = 0;
+        MyList myList;
+
+        public MyEnumerator(MyList myList)
+        {
+            this.myList = myList;
+        }
+
+        public object Current => myList[index - 1];
+
+        public bool MoveNext()
+        {
+            if (index + 1 > myList.Count)
+            {
+                index = 1;
+                return false;
+            }
+            else
+            {
+                index++;
+                return true;
+            }
+        }
+    }
+}
---
+++ Advice17/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace Advice17
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // 使用接口IMyEnumerable代替MyList
+            IMyEnumerable list = new MyList();
+
+            // 得到迭代器， 在循环中针对迭代器编码， 而不是集合MyList
+            IMyEnumerator enumerator = list.GetEnumerator();
+
+            for (int i = 0; i < list.Count; i++)
+            {
+                object current = enumerator.Current;
+                enumerator.MoveNext();
+            }
+
+            while (enumerator.MoveNext())
+            {
+                object current = enumerator.Current;
+            }
+        }
+    }
+}
