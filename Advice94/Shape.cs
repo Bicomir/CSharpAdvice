1177da584a59a92501cfa3f66afdeabc96800a88
bomir.wang <jinchunw385@gmail.com>
2021-12-26 18:36:09 +0800

    Add Advice 94 code

---
+++ Advice94/Shape.cs
@@ -0,0 +1,57 @@
+﻿using System;
+
+namespace Advice94
+{
+    public class Shape
+    {
+        public virtual void MethodVirtual()
+        {
+            Console.WriteLine("base MethodVirtual call");
+        }
+
+        public void Method()
+        {
+            Console.WriteLine("base method call");
+        }
+    }
+
+    class Circle : Shape
+    {
+        public override void MethodVirtual()
+        {
+            Console.WriteLine("circle override MethodVirtual");
+        }
+    }
+
+    class Rectangle : Shape
+    {
+
+    }
+
+    class Triangle : Shape
+    {
+        public new void MethodVirtual()
+        {
+            Console.WriteLine("triangle new MethodVirtual");
+        }
+
+        public new void Method()
+        {
+            Console.WriteLine("triangle new Method");
+        }
+    }
+
+    class Diamond : Shape
+    {
+        public void MethodVirtual()
+        {
+            Console.WriteLine("Diamond default MethodVirtual");
+        }
+
+        public void Method()
+        {
+            Console.WriteLine("Diamond default Method");
+        }
+    }
+
+}
