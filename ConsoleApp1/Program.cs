69ffaa7006aeac444c906bb8cc13663bdcebecc8
bomir.wang <jinchunw385@gmail.com>
2022-01-02 19:24:04 +0800

    Add Advice stage1 code

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Advice33
+{
+    public class MyList<T>
+    {
+        public static int Count { get; set; }
+
+        public MyList()
+        {
+            Count++;
+        }
+    }
+
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            MyList<int> myList1 = new MyList<int>();
+            MyList<int> myList2 = new MyList<int>();
+
+            MyList<string> myList3 = new MyList<string>();
+
+            Console.WriteLine(MyList<int>.Count);
+            Console.WriteLine(MyList<string>.Count);
+            Console.ReadLine();
+        }
+    }
+}
