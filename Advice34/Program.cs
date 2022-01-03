8dda1d4fa54ac55b3dd0b202bd5a3ac0cf68319a
bomir.wang <jinchunw385@gmail.com>
2022-01-03 15:03:58 +0800

    Adivce34 case1 code

---
+++ Advice34/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Advice34
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+
+    public class Salary
+    {
+        public string Name { get; set; }
+
+        public int BaseSalary { get; set; }
+
+        public int Bonus { get; set; }
+    }
+
+    public class SalaryComputer
+    {
+        public int Compare<T>(T t1, T t2)
+        {
+            return 0;
+        } 
+    }
+}
