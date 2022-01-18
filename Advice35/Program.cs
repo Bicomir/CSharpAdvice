c39698fb38b23fd14e65483e714a78a0fc9aa1d2
bomir.wang <jinchunw385@gmail.com>
2022-01-18 15:35:57 +0800

    Advice35 code demo

---
+++ Advice35/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Advice35
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            var list = new List<int>() { 1,2,2,3,3,3,3,35,56,7};
+            list.Find(v => v > 6);
+        }
+    }
+
+    public T Func<T>()
+    {
+        //T t = null;
+        //return t;
+
+        //T t = 0;
+        //return t;
+
+        T t = default(T);
+        return t;
+    }
+}
