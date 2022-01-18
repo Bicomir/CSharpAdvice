358b35e54af12f76fb71cbeb9dee1643588cf9f3
bomir.wang <jinchunw385@gmail.com>
2022-01-18 15:55:56 +0800

    Advice36 case1 code

---
+++ Advice36/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Advice36
+{
+    internal class Program
+    {
+        delegate int AddHandler(int i, int j);
+        delegate void PrintHandler(string msg);
+
+        static void Main(string[] args)
+        {
+            AddHandler add = Add;
+            PrintHandler print = Print;
+
+            print(add(1, 2).ToString());
+        }
+
+        static int Add(int i, int j)
+        {
+            return i + j;
+        }
+
+        static void Print(string msg)
+        {
+            Console.WriteLine(msg);
+        }
+    }
+}
