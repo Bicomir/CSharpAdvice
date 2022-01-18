e2eec6f1c066cf54aff69d91a1e43e362926aea8
bomir.wang <jinchunw385@gmail.com>
2022-01-18 16:36:33 +0800

    代码的改写Step1

---
+++ Advice37/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Advice37
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Func<int, int, int> add = new Func<int, int, int>(Add);
+            Action<string> print = new Action<string>(Print);
+
+            print(add(1, 2).ToString());
+        }
+
+        static int Add(int i, int j)
+        {
+            return i + j;
+        }
+
+        static void Print(string msg)
+        {
+            Console.WriteLine(msg);
+        }
+    }
+}
