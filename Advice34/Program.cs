d426ceec8aa8acb209ce8211acfc4dacc6539224
bomir.wang <jinchunw385@gmail.com>
2022-01-22 17:47:31 +0800

    complete Advice34 computer

--- Advice34/Program.cs
+++ Advice34/Program.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Advice34
+﻿namespace Advice34
 {
     public class Program
     {
@@ -25,9 +19,20 @@ namespace Advice34
 
     public class SalaryComputer
     {
-        public int Compare<T>(T t1, T t2)
+        public int Compare<T>(T t1, T t2) where T : Salary
         {
-            return 0;
+            if (t1.BaseSalary > t2.BaseSalary)
+            {
+                return 1;
+            }
+            else if (t1.BaseSalary == t2.BaseSalary)
+            {
+                return 0;
+            }
+            else
+            {
+                return -1;
+            }
         } 
     }
 }
