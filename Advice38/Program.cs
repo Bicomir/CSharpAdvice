23618266d4691b2873e4f80ce26d72fe01e240a3
bomir.wang <jinchunw385@gmail.com>
2022-01-25 17:19:43 +0800

    Advice38 step1 code

---
+++ Advice38/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Advice38
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Action> lists = new List<Action>();
+            for (int i = 0; i < 5; i++)
+            {
+                Action t = () =>
+                {
+                    Console.WriteLine(i.ToString());
+                };
+                lists.Add(t);
+            }
+
+            foreach (var t in lists)
+            {
+                t();
+            }
+        }
+    }
+}
