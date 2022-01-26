feddd2ae962d3738a0c6f5998a8d4c3b045a6a27
bomir.wang <jinchunw385@gmail.com>
2022-01-26 18:06:52 +0800

    Advice39 code

---
+++ Advice39/FileUploadedHandler.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Advice39
+{
+    class FileUploadedHandler : MulticastDelegate
+    {
+        public FileUploadedHandler(object @object, IntPtr intPtr)
+        {
+
+        }
+
+        public virtual IAsyncResult BeginInvoke(int progress, AsyncCallback callback, object @object)
+        {
+
+        }
+
+        public virtual void EndInvoke(IAsyncResult result)
+        {
+
+        }
+
+        public virtual void Invoke(int progress)
+        {
+
+        }
+    }
+}
---
+++ Advice39/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Advice39
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
