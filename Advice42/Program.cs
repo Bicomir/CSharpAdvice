8d71451cda2340949284cda94ba458afec148f97
bomir.wang <jinchunw385@gmail.com>
2022-01-27 11:12:46 +0800

    Advice42 first code

---
+++ Advice42/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace Advice42
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ISalary<Programmer> s = new BaseSalaryCounter<Programmer>();
+            PrintSalary(s);
+            Console.ReadLine();
+        }
+
+        static void PrintSalary<T>(ISalary<T> s)
+        {
+            s.Pay();
+        }
+    }
+
+    interface ISalary<T>
+    {
+        void Pay();
+    }
+
+
+    class BaseSalaryCounter<T> : ISalary<T>
+    {
+        public void Pay()
+        {
+            Console.WriteLine("Pay base salary");
+        }
+    }
+
+    class Employee
+    {
+        public string Name { get; set; }
+    }
+
+    class Programmer : Employee
+    {
+
+    }
+
+    class Manager : Employee
+    {
+
+    }
+}
